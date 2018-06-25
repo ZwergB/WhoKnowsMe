c472f70b03c79779be2b46a743951a51926037c1
ZwergB <ben@zwergb.de>
2018-06-25 17:27:22 +0200

    Changed some scripting issues

--- WhoKnowsMe/src/main/webapp/script.js
+++ WhoKnowsMe/src/main/webapp/script.js
@@ -1,57 +1,68 @@
-jQuery(function() {
-
-function start() {
+function submitClick() {
 	//1) output mit Ladebalken ersetzen (optional)
-	var reloading = document.createElement('div');
+	let reloading = document.createElement('div');
 	reloading.setAttribute('id','reloading');
-	jQuery('#output').prepend(reloading);
+	document.getElementById('output').innerHTML = '';
+	document.getElementById('output').appendChild(reloading);
 	
 	//2) Eingabe auslesen //wenn eingabe leer, hier abbrechen (aber ladebalken wieder entfernen)
-	var input = jQuery('#username').val();
+	let input = document.getElementById('username').value;
 
 	//3) Anfrage senden (findAccounts/EINGABE)
-	jQuery.getJSON('services/AccountService/serve/'+input, runRequest(input));
+	let url = 'services/AccountService/serve/' + input;
+
+	webRequest('GET', url)
+	// when the request is successful
+	  .then(j => runRequest(input, JSON.parse(j)))
+	// when the request fails
+	  .catch(e => errorRequest(e));
 }
-	
-var runRequest = function(accountname) {
-	var subheader = createSubheader(accountname);
-	
-	return function(json) {
-		//4) antwort (json) in liste umformen
-		var list = createList(json);
-
-		//5) output.innerHTML durch Liste ersetzen
-		jQuery('#output').empty();
-		jQuery('#output').append(subheader);
-		jQuery('#output').append(list);
-	};
-};
-	
-
-
 
 //====INPUT=EVENTS==============
 
-//this function will be run when enter is pressed in the #username input
-jQuery('#username').keypress(function (e) {
-	if (e.which == 13) {
-   		start();
-   	}
-});
-
-//this function will run when button #submit is clicked
-jQuery('#submit').click(function() {
-	start();	
-});
-
-
+window.onload = function() {
+	// pseudo button press, when you press enter while focusing the nickname input field
+	this.document.getElementById('username').addEventListener('keypress', function(e) {
+		if (e.keyCode == 13) {
+			submitClick();
+		}
+	})
+}
 
 //====HELPER=FUNCTIONS========
 
-function createSubheader(accountname) {
-	var subheader = document.createElement('h2');
-	subheader.innerHTML = 'There are accounts for "' + accountname + '" at:"';
-	return subheader;
+function runRequest(username, json) {
+	let subheader       = document.createElement('h2');
+	subheader.innerHTML = 'There are accounts for "' + username + '" at:';
+
+	let list = createList(json);
+
+	let output = document.getElementById('output');
+
+	// Clear divs content
+	output.innerHTML = '';
+
+	output.appendChild(subheader);
+	output.appendChild(list);
+};
+
+function errorRequest(error) {
+	let subheader       = document.createElement('h2');
+	subheader.innerHTML = 'Oops, it seems like something went wrong.';
+
+	let output = document.getElementById('output');
+	output.innerHTML = '';
+
+	output.appendChild(subheader);	
+
+	// Displays the full error log
+	for (const prop in error) {
+		if (error.hasOwnProperty(prop)) {
+			let pNode = document.createElement('p');
+			pNode.innerHTML = error[prop];
+			output.appendChild(pNode);
+		}
+	}	
 }
 
 function createList(json) {
@@ -62,15 +73,52 @@ function createList(json) {
 	// <li><a href="URL">account</a> on PORTAL_NAME</li>
 	//</ul>
 
-	var list = document.createElement('ul');
+	let list = document.createElement('ul');
 
-	for(var acc in json) {
-		var listPoint = document.createElement('li');
-		listPoint.innerHTML = '<a href="' + json[acc].url + '">' + json[acc].name + '</a>';
+	for(let acc in json) {
+		let listPoint = document.createElement('li');
+		console.log(json[acc])
+		
+		let aNode = createRefNode(json[acc].name, json[acc].url);
+		listPoint.appendChild(aNode);
 		list.append(listPoint);
 	}
 
 	return list;
+
+	function createRefNode(text, url) {
+		let node = document.createElement('a');
+		node.setAttribute('href', url);
+		node.innerHTML = text;
+
+		return node;
+	}
 }
 
-});
+function webRequest(method, url) {
+	return new Promise(function(resolve, reject) {
+
+		let xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function() {
+
+			// Good statuscodes, we can continue
+			if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					url: this.responseURL,
+					status: this.status,
+					statusText: this.statusText
+				});
+			}
+		};
+		xhr.onerror = function() {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText	
+			})
+		}
+		xhr.send();
+	});	
+}
